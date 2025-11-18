3e6683f7c99ffb0d81d56d7ccc2d2ef5896ae56a
ikerg5 <iker.gonzaleztorre@gmail.com>
2025-11-18 13:33:27 -0500

    Initial Xcode project setup

---
+++ Sendable/Sendable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sendable
+//
+//  Created by Iker Gonzalez on 11/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
