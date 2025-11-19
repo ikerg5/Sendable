1af779feaeae5cfaacf624b02d644fd924fecfa0
ikerg5 <iker.gonzaleztorre@gmail.com>
2025-11-19 00:37:56 -0500

    Uopdated main storyboard

---
+++ Sendable/Sendable/DetailedFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailedFeedViewController.swift
+//  Sendable
+//
+//  Created by Iker Gonzalez on 11/18/25.
+//
+
+import UIKit
+
+class DetailedFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
